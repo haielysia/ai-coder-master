0d95675e4cdda536c917e3cc9d9ec22c875711a2
itcast <2485183526@qq.com>
2026-06-15 14:14:04 +0800

    初始

---
+++ src/main/java/com/ai/aicodermaster/AiCoderMasterApplication.java
@@ -0,0 +1,13 @@
+package com.ai.aicodermaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiCoderMasterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiCoderMasterApplication.class, args);
+    }
+
+}
