932dbc6410016cd4433321897b697d4a03953cbd
itcast <2485183526@qq.com>
2026-06-16 18:07:41 +0800

    门面模式，生成并保存文件

---
+++ src/test/java/com/ai/aicodermaster/ai/AiCodeGeneratorFacadeTest.java
@@ -0,0 +1,24 @@
+package com.ai.aicodermaster.ai;
+
+import com.ai.aicodermaster.core.AiCodeGeneratorFacade;
+import com.ai.aicodermaster.model.enums.CodeGenTypeEnum;
+import jakarta.annotation.Resource;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.File;
+
+@SpringBootTest
+class AiCodeGeneratorFacadeTest {
+
+    @Resource
+    private AiCodeGeneratorFacade aiCodeGeneratorFacade;
+
+    @Test
+    void generateAndSaveCode() {
+        File file = aiCodeGeneratorFacade.generateAndSaveCode("任务记录网站", CodeGenTypeEnum.MULTI_FILE);
+        Assertions.assertNotNull(file);
+    }
+}
+
