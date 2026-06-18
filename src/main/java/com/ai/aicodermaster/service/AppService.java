3b1a882b21bbb90a8d0d2a08c730b1eab5b10553
itcast <2485183526@qq.com>
2026-06-18 16:52:18 +0800

    Vibe Coding + 二次审查。包括创建及更新应用，用户删除应用、查看详情、分页查询，管理员删除应用、查看详情、分页查询

--- src/main/java/com/ai/aicodermaster/service/AppService.java
+++ src/main/java/com/ai/aicodermaster/service/AppService.java
@@ -1,7 +1,14 @@
 package com.ai.aicodermaster.service;
 
+import com.ai.aicodermaster.model.dto.AppAdminQueryRequest;
+import com.ai.aicodermaster.model.dto.AppQueryRequest;
+import com.ai.aicodermaster.model.entity.User;
+import com.ai.aicodermaster.model.vo.AppVO;
+import com.mybatisflex.core.query.QueryWrapper;
 import com.mybatisflex.core.service.IService;
 import com.ai.aicodermaster.model.entity.App;
+
+import java.util.List;
 
 /**
  * 应用 服务层。
@@ -10,4 +17,26 @@ import com.ai.aicodermaster.model.entity.App;
  */
 public interface AppService extends IService<App> {
 
+    /**
+     * 获取脱敏的应用信息
+     *
+     * @param app 应用
+     * @return 脱敏后的应用信息
+     */
+    AppVO getAppVO(App app);
+
+    /**
+     * 获取脱敏的应用列表
+     *
+     * @param appList 应用列表
+     * @return 脱敏后的应用列表
+     */
+    List<AppVO> getAppVOList(List<App> appList);
+
+    /**
+     * 查询
+     * @param appQueryRequest
+     * @return
+     */
+    QueryWrapper getQueryWrapper(AppQueryRequest appQueryRequest);
 }
