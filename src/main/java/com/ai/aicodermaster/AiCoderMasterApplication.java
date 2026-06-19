9c2b9794d07c8540fd6ce8160d45ff322d59aab1
itcast <2485183526@qq.com>
2026-06-19 14:39:36 +0800

    对话记忆：配置Redis

--- src/main/java/com/ai/aicodermaster/AiCoderMasterApplication.java
+++ src/main/java/com/ai/aicodermaster/AiCoderMasterApplication.java
@@ -1,11 +1,12 @@
 package com.ai.aicodermaster;
 
+import dev.langchain4j.community.store.embedding.redis.spring.RedisEmbeddingStoreAutoConfiguration;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @MapperScan("com.ai.aicodermaster.mapper")
-@SpringBootApplication
+@SpringBootApplication(exclude = {RedisEmbeddingStoreAutoConfiguration.class})
 public class AiCoderMasterApplication {
 
     public static void main(String[] args) {
