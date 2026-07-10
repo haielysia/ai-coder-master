629bf5a68290d9c7938598d87656d0c7ebc828e5
itcast <2485183526@qq.com>
2026-07-10 14:22:42 +0800

    图片计划节点，内容图片手机节点，插画收集节点

--- src/main/java/com/ai/aicodermaster/langgraph4j/state/WorkflowContext.java
+++ src/main/java/com/ai/aicodermaster/langgraph4j/state/WorkflowContext.java
@@ -1,5 +1,6 @@
 package com.ai.aicodermaster.langgraph4j.state;
 
+import com.ai.aicodermaster.langgraph4j.model.ImageCollectionPlan;
 import com.ai.aicodermaster.langgraph4j.model.ImageResource;
 import com.ai.aicodermaster.langgraph4j.model.QualityResult;
 import com.ai.aicodermaster.model.enums.CodeGenTypeEnum;
@@ -77,6 +78,20 @@ public class WorkflowContext implements Serializable {
      * 质量检查结果
      */
     private QualityResult qualityResult;
+
+    /**
+     * 图片收集计划
+     */
+    private ImageCollectionPlan imageCollectionPlan;
+
+
+    /**
+     * 并发图片收集的中间结果字段
+     */
+    private List<ImageResource> contentImages;
+    private List<ImageResource> illustrations;
+    private List<ImageResource> diagrams;
+    private List<ImageResource> logos;
 
 
     @Serial
