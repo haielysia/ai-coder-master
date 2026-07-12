e8c936070283b39debb53022ff6ebe3c329a3215
itcast <2485183526@qq.com>
2026-07-12 15:46:50 +0800

    common公共模块

---
+++ ai-code-master-microservice/ai-code-common/config/RedisChatMemoryStoreConfig.java
@@ -0,0 +1,37 @@
+package com.ai.aicodermaster.config;
+
+import com.ai.aicodermaster.ai.AiCodeGeneratorService;
+import dev.langchain4j.community.store.memory.chat.redis.RedisChatMemoryStore;
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "spring.data.redis")
+@Data
+public class RedisChatMemoryStoreConfig {
+
+    private String host;
+
+    private int port;
+
+    private String password;
+
+    private String username;
+
+    private long ttl;
+
+    @Bean
+    public RedisChatMemoryStore redisChatMemoryStore() {
+        return RedisChatMemoryStore.builder()
+                .user(username)
+                .host(host)
+                .port(port)
+                .password(password)
+                .ttl(ttl)
+                .build();
+    }
+
+}
+
